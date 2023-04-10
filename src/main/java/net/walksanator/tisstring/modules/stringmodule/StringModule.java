83f8d60ca5ee04dee9017b3dafe8629cd51c7128
walksanatora <walkerffo22@gmail.com>
2023-04-10 01:00:55 -0700

    yknow what, state enum for turning numbers -> strings just gonna leave that 'async'

--- src/main/java/net/walksanator/tisstring/modules/stringmodule/StringModule.java
+++ src/main/java/net/walksanator/tisstring/modules/stringmodule/StringModule.java
@@ -93,7 +93,7 @@ public class StringModule extends AbstractModuleWithRotation {
             if (!receivingPipe.isReading()) {
                 receivingPipe.beginRead();
             }
-            if (receivingPipe.canTransfer() && (state == STATE.AWAITING_INPUT) ) {
+            if (receivingPipe.canTransfer() ) {
                 switch (this.mode) {
                     case INT -> {
                         outbuf.append(receivingPipe.read());
